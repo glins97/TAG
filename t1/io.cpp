8840220bef69a84077f9206c2a848b5e0787336b
Gabriel Lins <gabriel.lins97@gmail.com>
2019-08-26 20:51:10 -0300

    added file loading

--- t1/io.cpp
+++ t1/io.cpp
@@ -1,2 +1,38 @@
 #include "io.hpp"
 
+vector<list<int>> loadGraph(string filename){
+    vector<list<int>> graph;
+
+    ifstream file(filename);
+    int n1, n2, edges;
+    file >> n1 >> n2 >> edges;
+    
+    for (int i = 0; i < n1; i++){
+        graph.push_back(list<int>());
+    }
+
+    int v1, v2;
+    for (int i = 0; i < edges; i++){
+        file >> v1 >> v2;
+        graph[v2-1].push_back(v1-1);
+        graph[v1-1].push_back(v2-1);
+    }
+
+    return graph;
+}
+
+void printList(list<int> l){
+    for (int i: l){
+        cout << i + 1 << " -> ";
+    }
+    cout << endl;
+}
+
+void printGraph(vector<list<int>> graph){
+    int count = 0;
+    for (list<int>l : graph){
+        count++;
+        cout << "[" << count << "] -> ";
+        printList(l); 
+    }
+}
--- t1/io.hpp
+++ t1/io.hpp
@@ -3,3 +3,11 @@
 #include <iostream>
 #include <string>
 #include <list>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+vector<list<int>> loadGraph(string filename);
+void printList(list<int> l);
+void printGraph(vector<list<int>> graph);
