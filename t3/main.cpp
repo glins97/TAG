0998ff0f2b11c971343a7f45f0d2d37fee4cd29f
Gabriel Lins <gabriel.lins97@gmail.com>
2019-11-01 22:50:35 -0300

    Separate implementation of gale shapley

--- t3/main.cpp
+++ t3/main.cpp
@@ -1,3 +1,4 @@
+#include <map>
 #include "src/io.hpp"
 
 using namespace std;
@@ -12,10 +13,12 @@ T getObjById(vector<T> v, string id){
 }
 
 template <class T>
-bool in(vector<T> v, T obj){
+int in(vector<T> v, T obj){
+    int i = 1;
     for (auto o: v)
-        if (o == obj) return true;
-    return false;
+        if (o == obj) return i;
+        else i++;
+    return 0;
 }
 
 void printSchools(vector<School> schools, vector<Teacher> teachers){
@@ -80,7 +83,6 @@ vector<Teacher> freeTeachersThatHaventProposed(vector<Teacher> teachers, int* co
     return candidates;
 }
 
-
 void propose(Teacher* teacher, School* school, bool successfuly, int preference){
     teacher->propostions->push_back(school->id);
     if (teacher->propostions->size() == teacher->preferences->size())
@@ -98,19 +100,15 @@ void propose(Teacher* teacher, School* school, bool successfuly, int preference)
 
 }
 
-int main(){
-    auto schools = loadSchools("resources/output_schools");
-    auto teachers = loadTeachers("resources/output_teachers");
-
+void galeShapley(vector<Teacher>* teachers, vector<School>* schools){
     int count = 0;
-    int i = 1;
-    auto freeTeachers = freeTeachersThatHaventProposed(teachers, &count);
+    auto freeTeachers = freeTeachersThatHaventProposed(*teachers, &count);
     while (count){
         School school = School();
         Teacher teacher = freeTeachers.back();
         freeTeachers.pop_back();
 
-        int hasUnproposedSchool = getUnproposedSchool(teacher, schools, &school);
+        int hasUnproposedSchool = getUnproposedSchool(teacher, *schools, &school);
         if (hasUnproposedSchool){
             // School is not matched, and current teacher is a match
             if (school.chosenTeacherP1 == "" &&
@@ -118,25 +116,24 @@ int main(){
                 propose(&teacher, &school, true, 1);
             }
             else if (school.chosenTeacherP2 == "" &&
-                    school.p2 > 0 &&
-                    teacher.hab >= school.p2){
+                     school.p2 > 0 &&
+                     teacher.hab >= school.p2){
                 propose(&teacher, &school, true, 2);
             }
 
             // School is matched, but current teacher is a better match
             else if(school.chosenTeacherP1 != "" &&
-                    teacher.hab > getObjById(teachers, school.chosenTeacherP1).hab) {
-                auto lastTeacher = getObjById(teachers, school.chosenTeacherP1);
+                    teacher.hab > getObjById(*teachers, school.chosenTeacherP1).hab) {
+                auto lastTeacher = getObjById(*teachers, school.chosenTeacherP1);
                 lastTeacher.chosenSchool = "";
-                update(&teachers, lastTeacher);
+                update(teachers, lastTeacher);
                 propose(&teacher, &school, true, 1);
             }
             else if(school.chosenTeacherP2 != "" &&
-                    school.p2 > 0 &&
-                    teacher.hab > getObjById(teachers, school.chosenTeacherP2).hab) {
-                auto lastTeacher = getObjById(teachers, school.chosenTeacherP2);
+                    teacher.hab > getObjById(*teachers, school.chosenTeacherP2).hab) {
+                auto lastTeacher = getObjById(*teachers, school.chosenTeacherP2);
                 lastTeacher.chosenSchool = "";
-                update(&teachers, lastTeacher);
+                update(teachers, lastTeacher);
                 propose(&teacher, &school, true, 2);
             }
 
@@ -144,48 +141,52 @@ int main(){
             else{
                 propose(&teacher, &school, false, -1);
             }
-            update(&teachers, teacher);
-            update(&schools, school);
+            update(teachers, teacher);
+            update(schools, school);
         }
-        freeTeachers = freeTeachersThatHaventProposed(teachers, &count);
+        freeTeachers = freeTeachersThatHaventProposed(*teachers, &count);
     }
-
-    int allocatedTeachers =  printTeachers(teachers);
-    cout << "Professores empregados: " << allocatedTeachers << endl;
-    printSchools(schools, teachers);
-    return 0;
 }
+
 
 void checkForCorrectness(vector<School> schools, vector<Teacher> teachers){
     // check if there is any teacher available that chould be matched with another school
     for (auto school: schools){
-        Teacher chosenTeacherP1;
-        if (school.chosenTeacherP1 != "") chosenTeacherP1 = getObjById(teachers, school.chosenTeacherP1);
-        Teacher chosenTeacherP2;
-        if (school.chosenTeacherP2 != "") chosenTeacherP2 = getObjById(teachers, school.chosenTeacherP2);
+        int habp1 = 3;
+        if (school.chosenTeacherP1 != "") habp1 = getObjById(teachers, school.chosenTeacherP1).hab;
+        int habp2 = 3;
+        if (school.chosenTeacherP2 != "") habp2 = getObjById(teachers, school.chosenTeacherP2).hab;
 
         for (auto teacher: teachers){
             bool failed = false;
-            if (teacher.chosenSchool == "" && in(*(teacher.preferences), school.id)){
-                if( teacher.hab >= school.p1 &&
-                    school.chosenTeacherP1 != "" &&
-                    teacher.hab > chosenTeacherP1.hab &&
-                    school.p1 != 0){
-                    failed = true;
-                }
-                if( teacher.hab >= school.p2 &&
-                    school.chosenTeacherP2 != "" &&
-                    teacher.hab > chosenTeacherP2.hab &&
-                    school.p2 != 0){
-                    failed = true;
-                }
-            }
-            if (failed){
+            int currentPreferenceIndex = in(*(teacher.preferences), teacher.chosenSchool);
+            int schoolPreferenceIndex = in(*(teacher.preferences), school.id);
+
+            if (schoolPreferenceIndex < currentPreferenceIndex && schoolPreferenceIndex > 0 &&
+                    (teacher.hab > habp2 || teacher.hab > habp1))
+            {
                 cout << "[ALGORITHM FAILED] We have a teacher match:\n" << teacher.repr() << endl;
-                cout << "\t" << school.repr() << endl;
-                cout << "\t" << "School P1: " << chosenTeacherP1.repr() << endl;
-                cout << "\t" << "School P2: " << chosenTeacherP2.repr() << endl;
+                cout << schoolPreferenceIndex << " " << currentPreferenceIndex << endl;
+                cout << school.repr() << endl;
+                if (school.chosenTeacherP1 != "")
+                    cout << "\t" << "School P1: " << getObjById(teachers, school.chosenTeacherP1).repr() << endl;
+
+                if (school.chosenTeacherP2 != "")
+                cout << "\t" << "School P2: " << getObjById(teachers, school.chosenTeacherP2).repr() << endl;
             }
         }
     }
+}
+
+int main(){
+    auto schools = loadSchools("resources/output_schools");
+    auto teachers = loadTeachers("resources/output_teachers");
+
+    galeShapley(&teachers, &schools);
+    checkForCorrectness(schools, teachers);
+
+    int allocatedTeachers = printTeachers(teachers);
+    cout << "Professores empregados: " << allocatedTeachers << endl;
+
+    return 0;
 }
